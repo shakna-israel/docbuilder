c4505ed1c99558ead27eb896511c1118aac9538f
Shakna Israel <s4b3r6@gmail.com>
2015-04-08 11:40:01 +0000

    Working PEP8 test that ignores correctly.

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ class TestCodeFormat(unittest.TestCase):
 
     def test_pep8_complicance(self):
         """Test that Docbuilder does comply with PEP8."""
-        pep8style = pep8.StyleGuide(quiet=False)
+        pep8style = pep8.StyleGuide(quiet=False,config_file="pep8.conf")
         result = pep8style.check_files(['docbuilder.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
