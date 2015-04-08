eaca87acc1586f013a15719866d00dc9b5e8e0c2
Shakna Israel <s4b3r6@gmail.com>
2015-04-08 08:05:11 +0000

    Turned quiet off pep8 test

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ class TestCodeFormat(unittest.TestCase):
 
     def test_pep8_complicance(self):
         """Test that we comply with PEP8."""
-        pep8style = pep8.StyleGuide(quiet=True)
+        pep8style = pep8.StyleGuide(quiet=False)
         result = pep8style.check_files(['docbuilder.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
