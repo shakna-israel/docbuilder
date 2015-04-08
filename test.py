0d6ecabdd6a1c1aac371fdd396266f7bf8af58e8
Shakna Israel <s4b3r6@gmail.com>
2015-04-08 08:15:08 +0000

    Grammatical change.

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import pep8
 class TestCodeFormat(unittest.TestCase):
 
     def test_pep8_complicance(self):
-        """Test that we comply with PEP8."""
+        """Test that Docbuilder does comply with PEP8."""
         pep8style = pep8.StyleGuide(quiet=False)
         result = pep8style.check_files(['docbuilder.py'])
         self.assertEqual(result.total_errors, 0,
