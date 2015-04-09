1b0a61c06aedb976c68d44f7dd3082e48ab0d24e
Shakna Israel <s4b3r6@gmail.com>
2015-04-09 14:30:54 +1000

    Added test_file_generation
    A quick test to check if Docbuilder does actually build documentation. Does not use arguments.

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,6 @@
 import unittest
 import pep8
+import os
 
 # A simple test using pep8 to check compliance
 class TestCodeFormat(unittest.TestCase):
@@ -10,3 +11,16 @@ class TestCodeFormat(unittest.TestCase):
         result = pep8style.check_files(['docbuilder.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
+
+    # A simple test to check if Docbuilder creates files and directories.                     
+    def test_file_generation(self):
+        """Test that Docbuilder can create it's own documentation."""
+        # This subprocess calls docbuilder, so we can see if it's building it's own documentation.
+        subprocess.call("python ../docbuilder.py", shell=True)
+        # Test if Docbuilder can build it's own documentation.
+        if os.path.isfile("docs/docbuilder.py.md):
+            print("Docbuilder successfully generated it's own documentation.")
+            pass
+        else:
+            print("Docbuilder failed to generate it's own documentation.")
+            assert False
