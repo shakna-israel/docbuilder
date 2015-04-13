e449bc400ac2a4d58d567613666f3fad1d710231
Shakna Israel <s4b3r6@gmail.com>
2015-04-13 07:42:40 +0000

    Basic Function Shapes

--- docbuilder.py
+++ docbuilder.py
@@ -10,9 +10,117 @@
 # * The options for Literate Programming under Python is few and far between.
 # * Impressive compatibility. It doesn't really matter what version of Python you are running. Docbuilder will run.
 
-## Notes:
+# # Notes:
 # * Docbuilder will not ask before clobbering your output file. It may even kill the entire directory.
 # * Docbuilder has no dependencies.
 # * Docbuilder simply converts your comments and code into Markdown.
 # * Docbuilder builds Markdown, and is mostly compatible with whatever [Python Markdown](https://pythonhosted.org/Markdown/) can read. But just in case, check the [Issues](https://github.com/shakna-israel/docbuilder/issues).
+
+# # Dependencies:
+# Used to read, write and check files.
+import os
+import sys
+
+# # Global Variables:
+# *FILE* is used to tell Docbuilder what file to build documentation for.
+global FILE
+# *DIRECTORY* is used to tell Docbuilder where to build documentation to.
+global DIRECTORY
+# *EXPORT* is used to tell DOcbuilder what file to build documentation into.
+global EXPORT
+# *verboseActive* is used to tell Docbuilder to say more on the commandline. A lot more.
+global verboseActive
+# *stringUnstripped* is used to tell Docbuilder what the current line of the file being processed looks like.
+global stringUnstripped
+# *stringStripped* tells Docbuilder what the current line of the file being process looks like without whitespace.
+global stringStripped
+# *firstChar* tell Docbuilder what the first character of the current line of the file being processed looks like.
+global firstChar
+# *compareChar* is a unicode symbol for the hash symbol, to prevent Python breaking Docbuilder's code.
+global compareChar
+# *outFile* tells Docbuilder what memory object is the file it is writing to.
+global outFile
+
+def fileReadFrom(input):
+    global FILE
+    try:
+        FILE = input
+    except IndexError:
+        if verboseActive:
+            print("No file provided to build documentation for.\nBuilding Docbuilder documentation.")
+        FILE = "docbuilder.py"
+
+def directoryWriteTo(dir):
+    global DIRECTORY
+    try:
+        DIRECTORY = dir + "/"
+    except IndexError:
+        if verboseActive:
+            print("No directory provided to build documentation into.\nBuilding into 'docs' directory.")
+        DIRECTORY = "docs/"
+
+def fileWriteTo(input):
+    global EXPORT
+    try:
+        EXPORT = DIRECTORY + "/" + input
+    except IndexError:
+        EXPORT = DIRECTORY + FILE + ".md"
+        if verboseActive:
+            print("No output file provided, guessing... " + EXPORT)
+
+def stringManage(line):
+    global stringUnstripped
+    global stringStripped
+    global firstChar
+    stringUnstripped = line
+    if verboseActive:
+        print("The current unstripped line is... " + stringUnstripped)
+    stringStripped = stringUnstripped.strip()
+    if verboseActive:
+        print("The current stripped line is... " + stringStripped)
+    firstChar = stringStripped[:1]
+    if verboseActive:
+        print("The first character of the current line is..." + firstChar)
+
+def checkExportFile(input):
+    if os.path.isfile(input):
+        os.remove(input)
+
+def checkExportDir(input):
+    if not os.path.exists(input):
+        os.makedirs(input)
+
+def unicodeCompareChar(input):
+    global compareChar
+    try:
+        compareChar = unichr(input)
+    except NameError:
+        compareChar = chr(input)
+
+def markdownWrite(input):
+    global outFile
+    outFile.write("\n")
+    outFile.write(input)
+    outFile.write("\n")
+
+def codeblockWrite(input):
+    global outFile
+    outFile.write("\n```\n")
+    outFile.write(input)
+    outFile.write("\n```\n")
+
+def readFile(input, output):
+    global outFile
+    inFile = open(input, "r")
+    outFile = open(output, "w+")
+    for lineRead in inFile.read().split("\n"):
+        stringManage(input)
+        unicodeCompareChar(35)
+        if firstChar == compareChar:
+            markdownWrite(stringStripped)
+        else:
+            if stringUnstripped != "":
+                codeblockWrite(stringUnstripped)
+    inFile.close()
+    outFile.close()
 
