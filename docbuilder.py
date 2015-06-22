2e48975f3f00c11d92056d630abae81e232aab7e
Shakna Israel <s4b3r6@gmail.com>
2015-06-22 13:25:22 +0000

    Switched to 0.5 for release

--- docbuilder.py
+++ docbuilder.py
@@ -30,7 +30,7 @@ import fileinput
 # These are used with setuptools and Pip to let people know what exactly they are installing.
 
 __author__ = 'James Milne'
-__version__ = '0.5-dev'
+__version__ = '0.5'
 __license__ = 'MIT'
 __description__ = 'Docbuilder allows you to build Markdown documents without the need for detangling executables from literate Python programs. http://docbuilder.rtfd.org'
 
