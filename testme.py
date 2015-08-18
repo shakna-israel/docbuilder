7c1c523165536f37dd09c45e344c124b2d5de3ac
James Milne <s4b3r6@gmail.com>
2015-08-18 12:05:07 +1000

    Better .gitignore

--- testme.py
+++
@@ -1,8 +0,0 @@
-# # This is a title. 
- # This is a comment. 
- # * This is a bullet point. 
- # *This* is an italic word. 
- # **This** is a bold word. 
- # ***This*** is an italic and bold word. 
- print('This should be in a code block.')
-    print('This is an indented code block.')
