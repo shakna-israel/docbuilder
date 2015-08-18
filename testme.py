3088f37aff741a6c19387a6debf3272296ffa4a0
James Milne <s4b3r6@gmail.com>
2015-08-18 12:03:54 +1000

    Added OO example file

---
+++ testme.py
@@ -0,0 +1,8 @@
+# # This is a title. 
+ # This is a comment. 
+ # * This is a bullet point. 
+ # *This* is an italic word. 
+ # **This** is a bold word. 
+ # ***This*** is an italic and bold word. 
+ print('This should be in a code block.')
+    print('This is an indented code block.')
